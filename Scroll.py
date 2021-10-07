f9703303075069bc5585e1084b0e426832b443a8
theMathR <56732467+theMathR@users.noreply.github.com>
2021-10-07 20:15:52 +0200

    Update Scroll.py
    Fixed an error

--- Scroll.py
+++ Scroll.py
@@ -1,6 +1,6 @@
 program = input()
 
-string = []  # Totally a string
+string = [] # Totally a string
 n = []
 
 # Find matching brackets
@@ -10,47 +10,49 @@ open_brackets = []
 
 index = 0
 while index < len(program):
-    if program[index] == "[":
-        open_brackets.append(index)
-    elif program[index] == "]":
-        left_brackets[index] = open_brackets[-1]
-        right_brackets[open_brackets[-1]] = index
-        del open_brackets[-1]
-    index = 1
+   if program[index] == "[":
+      open_brackets.append(index)
+   elif program[index] == "]":
+      left_brackets[index] = open_brackets[-1]
+      right_brackets[open_brackets[-1]] = index
+      del open_brackets[-1]
+   index += 1
 
 if open_brackets:
-    print("Error: Unexpected EOF")
-    exit(-1)
+   print("Error: Unexpected EOF")
+   exit(-1)
 
 # Execute the program
 index = 0
 while index < len(program):
-    command = program[index]
+   command = program[index]
 
-    if command == "[":
-        n.append(string[-1])
-        del string[-1]
+   try:
+      if command == '[':
+         n.append(string[-1])
+         del string[-1]
 
-        if string[-1] == n[-1]:
-            index = right_brackets[index]
+         if string[-1] == n[-1]:
+           index = right_brackets[index]
 
-    elif command == "]":
-        if string[-1] != n[-1]:
+      elif command == ']':
+         if string[-1] != n[-1]:
             index = left_brackets[index]
-        else:
+         else:
             del n[-1]
 
-    elif command == "$":
-        string.insert(0, string[-1])
-        del string[-1]
+      elif command == '$':
+         string.insert(0, string[-1])
+         del string[-1]
 
-    elif command == "*":
-        del string[-1]
+      elif command == '*':
+         del string[-1]
 
-    else:
-        string.append(command)
+      else:
+         string.append(command)
+   except IndexError: # Can append if the string or n is empty
+      pass
 
-    index = 1
+   index += 1
 
-print("".join(string))
-
+print(''.join(string))
